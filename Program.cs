82bb23babc7192c5eddd170b8f82b7fdbb017035
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-20 09:38:04 +0200

    3 задание

--- Program.cs
+++ Program.cs
@@ -5,17 +5,35 @@ namespace ConsoleApp32_Ignat_20._03
     class Program
     {
         static void Main(string[] args)
-        { 
-            // 2 zadanie
-            double[] heights = { 150, 160, 165, 168, 180, 172, 160, 185, 174, 190 };
-            double sumHeight = 0;
-            for (int i = 0; i < heights.Length; i++)
+        {
+            // 3 zadanie
+            int[] array = new int[15];
+            int positiveCount = 0, negativeCount = 0, zeroCount = 0;
+                Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
             {
-                sumHeight += heights[i];
+                array[i] = rnd.Next(-10, 11);
+                Console.Write(array[i] + " Ответ ");
+                if (array[i] > 0)
+                {
+                    positiveCount++;
+                }
+                else if (array[i] < 0)
+                {
+                    negativeCount++;
+                }
+                else
+                {
+                    zeroCount++;
+                }
+                Console.WriteLine();
+                Console.WriteLine($"Положительное кол-во элементов: { positiveCount}");
+                Console.WriteLine($"Отрицательное кол-во элементов: {negativeCount}");
+                Console.WriteLine($"Нулевое кол-во элементов: {zeroCount}");
+                Console.Read();
             }
-            double averageHeight = sumHeight / heights.Length;
-            Console.WriteLine($"Средний рост студентов группы: {averageHeight}");
-            Console.Read();
+
+
         }
     }
 }
