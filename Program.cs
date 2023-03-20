3dcde0f0a2e922038a25cf9fb305a05caa43ece6
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-20 09:15:47 +0200

    2 zadanie

--- Program.cs
+++ Program.cs
@@ -5,15 +5,16 @@ namespace ConsoleApp32_Ignat_20._03
     class Program
     {
         static void Main(string[] args)
-        {
-            // 1 zadanie
-            double[] mass = { 12.5, 20.3, 5.8, 8.7, 15.2 };
-            double totalMass = 0;
-            for (int i = 0; i < mass.Length; i++)
+        { 
+            // 2 zadanie
+            double[] heights = { 150, 160, 165, 168, 180, 172, 160, 185, 174, 190 };
+            double sumHeight = 0;
+            for (int i = 0; i < heights.Length; i++)
             {
-                totalMass += mass[i];
+                sumHeight += heights[i];
             }
-            Console.WriteLine($"Общая масса груза: {totalMass}");
+            double averageHeight = sumHeight / heights.Length;
+            Console.WriteLine($"Средний рост студентов группы: {averageHeight}");
             Console.Read();
         }
     }
