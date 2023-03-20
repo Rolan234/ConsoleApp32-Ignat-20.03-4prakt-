43e30a34f030568fa0401390e32c39833d826a08
Ahsim-Samish <s_bernaz@mail.ru>
2023-03-20 09:05:06 +0200

    1 - e  zadanie

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp32_Ignat_20._03
     {
         static void Main(string[] args)
         {
-            // 1 zadanie 
+            // 1 zadanie
             double[] mass = { 12.5, 20.3, 5.8, 8.7, 15.2 };
             double totalMass = 0;
             for (int i = 0; i < mass.Length; i++)
